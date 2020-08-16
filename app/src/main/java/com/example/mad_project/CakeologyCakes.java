37cc9e72a63a87bafa8eeafadaf609f1698028cb
Dewminikw <dkweerabahu@gmail.com>
2020-08-17 01:44:48 +0530

    This is my commit for completing layouts.

---
+++ app/src/main/java/com/example/mad_project/CakeologyCakes.java
@@ -0,0 +1,14 @@
+package com.example.mad_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CakeologyCakes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cakeology_cakes);
+    }
+}
