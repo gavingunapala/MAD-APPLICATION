f5d32b02d0e814f2e262dcf25323624db97b2055
gavin <gavingunapala06@gmail.com>
2020-09-18 10:50:17 +0530

    created button click events

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.mad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button createAccount;
+    private Button login;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize buttons.
+        createAccount = findViewById(R.id.create_account);
+        login  = findViewById(R.id.login_button);
+
+        //go to the create account acctivity.
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCreateAcount();
+            }
+        });
+
+        //go to the create user profile acctivity.
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openlogin();
+            }
+        });
+
+    }
+    public void openCreateAcount(){
+        Intent i =  new Intent(this,signIn.class);
+        startActivity(i);
+    }
+
+    public void openlogin(){
+        Intent i =  new Intent(this,UserProfile.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/mad_project/UserProfile.java
+++ app/src/main/java/com/example/mad_project/UserProfile.java
@@ -2,13 +2,33 @@ package com.example.mad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserProfile extends AppCompatActivity {
+    private Button home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        //Initialize buttons.
+        home= findViewById(R.id.backtohome);
+
+        //go to the create account acctivity.
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityhome();
+            }
+        });
+    }
+
+    public void openActivityhome(){
+        Intent i =  new Intent(this,Home.class);
+        startActivity(i);
     }
 }
