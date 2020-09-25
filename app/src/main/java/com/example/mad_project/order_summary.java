7c5647da8d1cf3ffa4006fa652425447112d40ca
Isira9827 <68735440+Isira9827@users.noreply.github.com>
2020-09-25 14:07:13 +0530

    added: add_more_details order_summary
    edited:
    sign_in

---
+++ app/src/main/java/com/example/mad_project/order_summary.java
@@ -0,0 +1,14 @@
+package com.example.mad_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class order_summary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_summary);
+    }
+}
